599f1512bf5cd3121a752d6a2406f8362d173c17
PCx200 <petko05raychinov@gmail.com>
2026-06-03 10:42:46 +0200

    Added Wind Stream functionality

---
+++ PuzzlePals/Assets/Scripts/WindStream.cs
@@ -0,0 +1,97 @@
+using JetBrains.Annotations;
+using Mono.Cecil.Cil;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class WindStream : MonoBehaviour
+{
+    public enum Direction { Froward, Backward, Up, Down }
+    [SerializeField] private Direction direction;
+
+    [SerializeField] private BoxCollider area;
+    [SerializeField] private float windSpeed;
+    [SerializeField] private Vector3 windSource;
+    private Vector3 windDirection;
+    private float windStrength;
+
+    private void OnValidate()
+    {
+        if (area == null)
+            area = GetComponent<BoxCollider>();
+
+        DetermineWindSource();
+        DetermineWindDirection();
+    }
+
+    void Start()
+    {
+
+    }
+
+    private void DetermineWindDirection()
+    {
+        switch (direction)
+        {
+            case Direction.Froward:
+                windDirection = Vector3.right;
+                break;
+            case Direction.Backward:
+                windDirection = Vector3.left;
+                break;
+            case Direction.Up:
+                windDirection = Vector3.up;
+                break;
+            case Direction.Down:
+                windDirection = Vector3.down;
+                break;
+            default:
+                break;
+        }
+    }
+    private void DetermineWindSource()
+    {
+        switch (direction)
+        {
+            case Direction.Froward:
+                windSource = new Vector3(transform.position.x - area.size.x / 2.0f, transform.position.y, transform.position.z);
+                break;
+            case Direction.Backward:
+                windSource = new Vector3(transform.position.x + area.size.x / 2.0f, transform.position.y, transform.position.z); 
+                break;
+            case Direction.Up:
+                windSource = new Vector3(transform.position.x, transform.position.y - area.size.y / 2.0f, transform.position.z);
+                break;
+            case Direction.Down:
+                windSource = new Vector3(transform.position.x, transform.position.y + area.size.y / 2.0f, transform.position.z);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            var rigidbody = other.GetComponent<Rigidbody>();
+
+            //updates the wind strength depending on how far the player is from the source
+            var distanceFromSource = windSource - other.transform.position;
+            windStrength = windSpeed / distanceFromSource.magnitude;
+
+            rigidbody.AddForce(windDirection * windStrength, ForceMode.Force);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        OnValidate();
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(windSource, 1f);
+        Gizmos.DrawWireCube(transform.position, area.size);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(windSource, transform.position);
+    }
+}
