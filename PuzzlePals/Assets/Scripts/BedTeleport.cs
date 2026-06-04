a2b5556814a0f6b318ebd46167497435cd78255f
PCx200 <petko05raychinov@gmail.com>
2026-06-04 11:31:38 +0200

    Transformation now can be either instant or with holding a button.  Added Bed teleportation logic.

---
+++ PuzzlePals/Assets/Scripts/BedTeleport.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class BedTeleport : MonoBehaviour, IInteractable
+{
+    [SerializeField] private BedTeleport linkedBed;
+
+    protected BedTeleport LinkedBed;
+
+    [SerializeField] private BoxCollider area;
+
+    private void OnValidate()
+    {
+        if (area == null)
+        {
+            area = GetComponent<BoxCollider>();
+        }
+    }
+
+    private void Start()
+    {
+        linkedBed.LinkedBed = this;
+    }
+
+    public void Interact()
+    {
+        var player = FindAnyObjectByType<PlayerController>();
+
+        if (player == null)
+        {
+            Debug.Log("Player is null");
+            return;
+        }
+
+        Teleport(player);
+    }
+    private void Teleport(PlayerController player)
+    {
+        player.transform.position = linkedBed.area.transform.position;
+    }
+
+    private void OnDrawGizmos()
+    {
+        OnValidate();
+
+        Gizmos.color = Color.darkBlue;
+        Gizmos.DrawWireCube(area.transform.position, area.size);
+
+        Gizmos.DrawLine(transform.position, linkedBed.transform.position);
+    }
+}
--- PuzzlePals/Assets/Scripts/TransformationPoint.cs
+++ PuzzlePals/Assets/Scripts/TransformationPoint.cs
@@ -3,10 +3,28 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider))]
 public class TransformationPoint : MonoBehaviour, IInteractable
 {
+    public enum TransformationType { Instant, WithInteraction }
+
+    [SerializeField] private TransformationType transformationType;
+
     [SerializeField] private MonsterCharacter characterToTransform;
     [SerializeField] BoxCollider area;
-    
-    void IInteractable.Interact()
+
+    //private void Awake()
+    //{
+    //    switch (transformationType)
+    //    {
+    //        case TransformationType.Instant:
+
+    //            break;
+    //        case TransformationType.WithInteraction:
+    //            break;
+    //        default:
+    //            break;
+    //    }
+    //}
+
+    public void Interact()
     {
         var player = FindAnyObjectByType<PlayerController>();
 
@@ -27,10 +45,19 @@ public class TransformationPoint : MonoBehaviour, IInteractable
 
     private void Transform(PlayerController player)
     {
+        var currentMonster = player.currentMonster;
+
+        var swappedMonster = currentMonster;
+
         player.currentMonster = characterToTransform;
+
+
         var newMonster = Instantiate(player.currentMonster.Prefab, player.transform.position, Quaternion.identity);
         Destroy(player.GetComponentInChildren<MonsterCharacter>().gameObject);
         newMonster.transform.SetParent(player.transform);
+
+        characterToTransform = swappedMonster;
+
         Debug.Log("Transformed into " + characterToTransform.Name);
     }
 
@@ -58,6 +85,16 @@ public class TransformationPoint : MonoBehaviour, IInteractable
                 break;
             default:
                 break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponent<PlayerController>();
+
+        if (player != null && transformationType == TransformationType.Instant)
+        {
+            Transform(player);
         }
     }
 }
