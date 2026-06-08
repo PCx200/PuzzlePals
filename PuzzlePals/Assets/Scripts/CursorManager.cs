820df56810f8bfdc1945181355aa5ce74f8d49cd
Rene <renehammink@icloud.com>
2026-06-08 14:39:52 +0200

    Audiomanager in the Greybox Level Scene
    AudioManager and CursorManager are now Prefabs and both singleton

--- PuzzlePals/Assets/Scripts/CursorManager.cs
+++ PuzzlePals/Assets/Scripts/CursorManager.cs
@@ -2,8 +2,21 @@ using UnityEngine;
 
 public class CursorManager : MonoBehaviour
 {
+    public static CursorManager Instance { get; private set; }
+    
     [SerializeField] private bool enableCursor;
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        } // Singleton
 
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    
 
     private void Start()
     {
